5857c116a3588d4d372304603c70360520213328
moults31 <moults31@gmail.com>
2023-05-13 13:16:19 -0700

    Add install script

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export ZSH="$HOME/.dots/oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+cp $HOME/.dots/.zsh-themes-moults31/* $HOME/.dots/oh-my-zsh/custom/themes
+sed -i 's/_THEME=\"robbyrussell\"/_THEME=\"circle\"/g' ~/.zshrc

b67ae1cd65cde265e3bdc13e84aae6987f1637f9
moults31 <moults31@gmail.com>
2023-05-13 16:06:20 -0700

    Add circle-no-git theme

---
+++ .zsh-themes-moults31/circle-no-git.zsh-theme
@@ -0,0 +1,2 @@
+PROMPT='%{$fg[white]%}$USER%(?,%{$fg[green]%},%{$fg[red]%}) ● %{$fg[white]%}'
+RPROMPT='%{$fg[blue]%}%2~%{$fg[white]%}%{$reset_color%}'
--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@
 export ZSH="$HOME/.dots/oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 cp $HOME/.dots/.zsh-themes-moults31/* $HOME/.dots/oh-my-zsh/custom/themes
-sed -i 's/_THEME=\"robbyrussell\"/_THEME=\"circle\"/g' ~/.zshrc
+sed -i 's/_THEME=\"robbyrussell\"/_THEME=\"circle-no-git\"/g' ~/.zshrc
